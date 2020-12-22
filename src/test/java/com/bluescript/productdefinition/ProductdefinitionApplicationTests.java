a481b1aec8afdbc472afb23da180b75d4b0d0103
BlueScript-Repo <repo@bluescripts.in>
2020-12-22 16:31:04 +0530

    commit for ProjectDefinition Service

---
+++ src/test/java/com/bluescript/productdefinition/ProductdefinitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluescript.productdefinition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdefinitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
